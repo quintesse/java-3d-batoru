ebc8b6400ad35589c19d62b45958718d6c25e638
tako <devnull@localhost>
2005-11-23 00:16:34 +0100

    [svn] Changed the code to use the new standard geometry utility classes. Adjusted the code to use the new jsr-231 api.

--- src/games/batoru/client/PatchyLandscapeRenderer.java
+++ src/games/batoru/client/PatchyLandscapeRenderer.java
@@ -2,10 +2,14 @@ package games.batoru.client;
 
 import javax.vecmath.*;
 
-import net.java.games.jogl.GL;
+import javax.media.opengl.GL;
 
 import org.codejive.utils4gl.RenderContext;
+import org.codejive.utils4gl.RenderObserver;
 import org.codejive.utils4gl.Renderable;
+import org.codejive.utils4gl.geometries.Geometry;
+import org.codejive.utils4gl.geometries.TriangleStripGeometry;
+import org.codejive.utils4gl.geometries.VertexBuffer;
 import org.codejive.world3d.Universe;
 
 import games.batoru.PatchyLandscape;
@@ -20,6 +24,7 @@ public class PatchyLandscapeRenderer implements Renderable {
 	
 	private float m_fTimeOfBirth;
 	private int m_nTerrainList;
+	private Geometry m_geo;
 	
 	public PatchyLandscapeRenderer(Universe _universe, PatchyLandscape _model) {
 		m_universe = _universe;
@@ -34,25 +39,30 @@ public class PatchyLandscapeRenderer implements Renderable {
 	}
 	
 	public void initRendering(RenderContext _context) {
-		updateRendering(_context);
-	}
-	
-	public void updateRendering(RenderContext _context) {
 		GL gl = _context.getGl();
-		m_nTerrainList = gl.glGenLists(1);
+		
+/*		m_nTerrainList = gl.glGenLists(1);
 		gl.glNewList(m_nTerrainList, GL.GL_COMPILE);
 		renderGeo(gl);
 		gl.glEndList();
+*/		
+		m_geo = createGeo(gl);
+		gl.glEnableClientState(GL.GL_VERTEX_ARRAY);
+		gl.glVertexPointer(3, GL.GL_FLOAT, 0, m_geo.getBuffer().getVertices());
+		// textures
+		gl.glEnableClientState(GL.GL_TEXTURE_COORD_ARRAY);
+		gl.glTexCoordPointer(2, GL.GL_FLOAT, 0, m_geo.getBuffer().getTexCoords());
 	}
 	
-	public void render(RenderContext _context) {
+	public void render(RenderContext _context, RenderObserver _observer) {
 		GL gl = _context.getGl();
 		
-		gl.glBindTexture(GL.GL_TEXTURE_2D, _context.getTextureHandle(0));
-		gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-		gl.glBegin(GL.GL_TRIANGLE_STRIP);
-		gl.glCallList(m_nTerrainList);
-		gl.glEnd();
+		_context.getTexture(0).bind();
+//		gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+//		gl.glBegin(GL.GL_TRIANGLE_STRIP);
+//		gl.glCallList(m_nTerrainList);
+//		gl.glEnd();
+		gl.glDrawArrays(GL.GL_TRIANGLE_STRIP, 0, m_geo.getBuffer().getSize());
 	}
 	
 	private void renderGeo(GL _gl) {
@@ -79,5 +89,34 @@ public class PatchyLandscapeRenderer implements Renderable {
 			}
 			bSwitchDirection = !bSwitchDirection;
 		}
+	}
+	
+	private Geometry createGeo(GL _gl) {
+		float vfHeights[][] = m_model.getHeights();
+		Point3f origin = m_model.getOrigin();
+		boolean bSwitchDirection = false;
+		int w = m_model.getWidth();
+		int h = m_model.getHeight();
+		int nFormat = VertexBuffer.COORDINATES | VertexBuffer.TEXTURES_2D | VertexBuffer.BUFFER_NIO;
+		VertexBuffer vb = new VertexBuffer(2 * w * (h + 1), nFormat);
+		for (int i = 0; i < w; i++) {
+			if (bSwitchDirection) {
+				for (int j = 0; j <= h; j++) {
+					vb.addTexCoord((float)(i + 1) / w, (float)j / h);
+					vb.addVertex(origin.x + (i + 1) * m_model.getPatchWidth(), origin.y + vfHeights[i + 1][j], origin.z + j * m_model.getPatchHeight());
+					vb.addTexCoord((float)i / w, (float)j / h);
+					vb.addVertex(origin.x + i * m_model.getPatchWidth(), origin.y + vfHeights[i][j], origin.z + j * m_model.getPatchHeight());
+				}
+			} else {
+				for (int j = h; j >= 0; j--) {
+					vb.addTexCoord((float)i / w, (float)j / h);
+					vb.addVertex(origin.x + i * m_model.getPatchWidth(), origin.y + vfHeights[i][j], origin.z + j * m_model.getPatchHeight());
+					vb.addTexCoord((float)(i + 1) / w, (float)j / h);
+					vb.addVertex(origin.x + (i + 1) * m_model.getPatchWidth(), origin.y + vfHeights[i + 1][j], origin.z + j * m_model.getPatchHeight());
+				}
+			}
+			bSwitchDirection = !bSwitchDirection;
+		}
+		return new TriangleStripGeometry(vb);
 	}
 }
